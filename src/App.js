26225921a17b0b1ed0d56503d75828f07f26edcf
NomadkingWild <67304919+NomadkingWild@users.noreply.github.com>
2020-08-21 11:42:27 -0400

    got all my imports done i think.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useHistory, Route, Switch, BrowserrRouter as Router} from 'react-router-dom';
+import axios from 'axios';
+import Toppings from './components/Toppings';
+import Pizza from './components/Pizza';
+import PizzaForm from './components/PizzaForm';
+import SavedPizza from './components/SavedPizza';
+
 
 const App = () => {
+  const [saved,  setSaved]= useState([]);
+  const[pizza, setPizza]=useState([]);
+  const history = useHistory()
+  const routeToHome = () =>{
+    history.push('/');
+  }
+  useEffect(()=>{
+    const getPizza = () => {
+      axios.get('(https://reqres.in/pizza)')
+      .then(res=>{
+        setPizza(res.data);
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+    }
+  })
   return (
     <>
       <h1>Lambda Eats</h1>
